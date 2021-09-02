a7ecfe41b4ed8176191a64b45f73c7ebdd7c009b
bhavi7023 <89994746+bhavi7023@users.noreply.github.com>
2021-09-02 21:44:04 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+
+$server="localhost";
+$username="id17526123_root";
+$password="&P5|-m6jJ?&i-IQl";
+$db="id17526123_fedaration";
+
+$conn=mysqli_connect($server,$username,$password,$db);
+
+if($conn){
+  
+}
+
+else
+die("connection to this database failed due to " .mysqli_connect_error());
+
+
+?>
